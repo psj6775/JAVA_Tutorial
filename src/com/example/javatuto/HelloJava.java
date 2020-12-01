168e5531d2dc0867ee710d704bb439d15d2d9df4
psj6775 <psj6775@naver.com>
2020-12-01 20:22:24 +0900

    Java Test

--- src/com/example/javatuto/HelloJava.java
+++ src/com/example/javatuto/HelloJava.java
@@ -2,6 +2,6 @@ package com.example.javatuto;
 
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello Java from intelliJ");
+        System.out.println("Hello~~~Java");
     }
 }
